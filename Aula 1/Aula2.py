e84775f9f1edaefd2d2161883a65f5589666b6d8
savioxavi67 <>
2025-08-05 10:33:41 -0400

    05/08/2025

---
+++ Aula 1/Aula2.py
@@ -0,0 +1,10 @@
+nome1 = 'Victor'
+nome2 = 'João'
+
+tamanhoNome2 = len(nome2)
+tamanhoNome1 = len(nome1)
+
+if tamanhoNome1 < tamanhoNome2:
+    print('Victor tem mais caractéres')
+else:
+    print('João tem mais caractéres')
