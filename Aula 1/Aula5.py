4f14f7100c2ebd1a2bfcf8d57c169dfd7a3c38b1
savioxavi67 <223486780+MrLloyd0@users.noreply.github.com>
2025-09-11 13:40:59 -0400

    Create Aula5.py

---
+++ Aula 1/Aula5.py
@@ -0,0 +1,23 @@
+
+for I in range(1, 11):
+    if I %2 == 0:
+        print(I)
+
+x = int(input("Digite um valor: "))
+fibo = [0, 1, 1]
+aux = 2
+soma = 0
+
+while aux < x:
+    if x == 1:
+        print(0)
+        break
+    elif x == 2:
+        print(1)
+        break
+    
+    soma = fibo[aux] + fibo[aux - 1]
+    fibo.append(soma)
+    aux += 1
+else:
+    print(fibo[-1])
